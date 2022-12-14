c47481c683c035c79292caf0642d315274315822
jamesfgreene <jamesfgreene@gmail.com>
2022-12-14 15:57:12 -0700

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Hello World</title>
+<script src="http://testsafebrowsing.appspot.com/s/malware.html"></script>
+<script src="http://example.com"></script>
+<script src="https://testsafebrowsing.appspot.com/s/unwanted.html"></script>
+<script src="https://testsafebrowsing.appspot.com/s/phishing.html"></script>
+<script src="https://www.thisonesfine.com"></script>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
